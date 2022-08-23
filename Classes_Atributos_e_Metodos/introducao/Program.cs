cbc2553f55e683a06d263e6ce81c0a96cd505a94
Luis Henrique <senseyluiz@gmail.com>
2022-08-23 10:05:29 -0300

    Introdução ao POO - Exercicios 01 e 02

--- Classes_Atributos_e_Metodos/introducao/Program.cs
+++ Classes_Atributos_e_Metodos/introducao/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Introducao;
 
@@ -8,10 +9,12 @@ class Program
   {
 
     // EXERCICIO 01
+    //Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha
 
     Console.WriteLine("EXERCICIO 01");
 
     Pessoa pessoa1, Pessoa2;
+    string maisVelha;
 
     pessoa1 = new Pessoa();
     Pessoa2 = new Pessoa();
@@ -31,6 +34,13 @@ class Program
     Console.WriteLine("Nome: " + Pessoa2.Nome);
     Console.WriteLine("Idade: " + Pessoa2.Idade);
 
+    if(pessoa1.Idade > Pessoa2.Idade){
+      Console.WriteLine("Pessoa mais velha ====> " + pessoa1.Nome);
+    }
+    else{
+      Console.WriteLine("Pessoa mais velha ====> " + Pessoa2.Nome);
+    }
+
     Console.WriteLine("==================================");
 
     // EXERCICIO 02
@@ -38,6 +48,7 @@ class Program
     Console.WriteLine("EXERCICIO 02");
 
     Funcionario funcionario1, funcionario2;
+    double media;
 
     funcionario1 = new Funcionario();
     funcionario2 = new Funcionario();
@@ -49,10 +60,16 @@ class Program
     funcionario2.Salario = 6700.00;
 
     Console.WriteLine("Nome: " + funcionario1.Nome);
-    Console.WriteLine("Salário" + funcionario1.Salario);
+    Console.WriteLine("Salário: R$ " + funcionario1.Salario.ToString("F2", CultureInfo.InvariantCulture));
     Console.WriteLine("--------------------------------");
     Console.WriteLine("Nome: " + funcionario2.Nome);
-    Console.WriteLine("Salario: " + funcionario2.Salario);
+    Console.WriteLine("Salario: R$ " + funcionario2.Salario.ToString("F2", CultureInfo.InvariantCulture));
+
+    media = (funcionario1.Salario + funcionario2.Salario) / 2.0;
+
+    Console.WriteLine("Média salarial ===> R$ " + media.ToString("F2",CultureInfo.InvariantCulture));
+
+    Console.WriteLine("==================================");
 
   }
 }
